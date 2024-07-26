b700b92df816a329f9a481881695f4d0abad55e8
Carlos_Pena <Carlos_Pena@EPCOBOGW1865>
2024-07-25 19:02:53 -0500

    adding cors

---
+++ Clients/CatalogClient.cs
@@ -0,0 +1,20 @@
+﻿using MusicStore.Client.Models;
+using System.Net.Http.Json;
+
+namespace MusicStore.Client.Clients
+{
+    public class CatalogClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Catalog[]?> GetAllCatalogsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<Catalog[]>("catalogs");
+        }
+    }
+}
